a866a52879ef3805019bcc83164bb49b47772b4c
Zelda Fan <zeldafan42@gmail.com>
2015-03-07 16:08:11 +0100

    Implemented Sessions(a bit)

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+	session_start();
+	if($_GET['logout'])
+	{
+		session_unset();
+		session_destroy();
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>	
@@ -11,20 +20,32 @@
 		<div id="wrapper">	
 			<div id="content">
 				<?php 
-					if(authenticateuser($_POST['username'],$_POST['password']))
-
+					if(!$_SESSION['loggedIn'])
+					{
+						authenticateuser($_POST['username'],$_POST['password']);
+					}
+					if($_SESSION['loggedIn'])
 					{
 						include("webtop.php");
 					}
 					else
 					{
+						if($_POST['login'])
+						{
+							echo "<p>Wrong username or password</p>";
+						}
 						include("login.php");
 					}
 					
+					echo "<a href=\"".$_SERVER['PHP_SELF']."?logout=true\">Logout</a>";
 					
 					function authenticateuser($user,$password)
 					{
-						return ($user == "username") && ($password == "password");
+						if(($user == "username") && ($password == "password"))
+						{
+							$_SESSION['username'] = $user;
+							$_SESSION['loggedIn'] = true;
+						}
 					}
 				?>
 			</div>
