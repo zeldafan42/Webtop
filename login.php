24ac4b7f2c2801693e0f5b4caef6fdae5f785010
Zombieranke <abcdefghijklmnop@qrs>
2015-03-08 18:11:22 +0100

    Added Icons for Login

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
 <form action="index.php" method="POST" id="loginForm">
 	<fieldset>
 		<legend>Login</legend>
-		<div class=loginIcon></div>
+		<div class=loginIcon><img src="res/zombie-icon.png" alt="Zombie-Icon damit man daneben den Benutzernamen eingibt"></div>
 		<input type="text" name="username" placeholder="Username"/>
-		<div class=loginIcon></div>
+		<div class=loginIcon><img src="res/lock-icon.png" alt="Schloss-Icon damit man daneben das Passwort eingibt"></div>
 		<input type="password" name="password" placeholder="Password"/>
 		<input type="submit" name="login" value="Login" id="loginButton"/>
 	</fieldset>
