bb1eb3e1addca6124527e2899bd3877dfd773914
Zelda Fan <zeldafan42@gmail.com>
2015-03-08 15:35:56 +0100

    Made the background work(kinda)

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 <form action="index.php" method="POST" id="loginForm">
 	<fieldset>
 		<legend>Login</legend>
-		<label>Username: <input type="text" name="username" /></label>
-		<label>Password: <input type="password" name="password" /></label>
+		<input type="text" name="username" value="Username"/>
+		<input type="password" name="password" value="Password"/>
 		<input type="submit" name="login" value="Login"/>
 	</fieldset>
 </form>
