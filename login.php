1681e55979960f37d92a054356fe59982bea904e
Zombieranke <abcdefghijklmnop@qrs>
2015-03-08 17:17:08 +0100

    Improved Login-design

--- login.php
+++ login.php
@@ -2,7 +2,9 @@
 <form action="index.php" method="POST" id="loginForm">
 	<fieldset>
 		<legend>Login</legend>
+		<div class=loginIcon></div>
 		<input type="text" name="username" placeholder="Username"/>
+		<div class=loginIcon></div>
 		<input type="password" name="password" placeholder="Password"/>
 		<input type="submit" name="login" value="Login" id="loginButton"/>
 	</fieldset>
