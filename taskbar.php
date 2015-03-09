d67744af0efcebd97f5f897a74816d20681a96b2
Zombieranke <abcdefghijklmnop@qrs>
2015-03-09 17:13:51 +0100

    there is only one ID taskbar anymore

--- taskbar.php
+++ taskbar.php
@@ -1,7 +1,6 @@
-<div id="taskbar">
-	<div id="startButton">
-		<a href="index.php?openMenu=true">
-			<img src="res/start-button.png">
-		</a>
-	</div>
+
+<div id="startButton">
+	<a href="index.php?openMenu=true">
+		<img src="res/start-button.png">
+	</a>
 </div>
