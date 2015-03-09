680d136359bece8dac4a7ae2ac380e7786aaf138
Zombieranke <abcdefghijklmnop@qrs>
2015-03-09 22:46:44 +0100

    last color change

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <!--<h1>Please Log in</h1>-->
 <form action="index.php" method="POST" id="loginForm">
 	<fieldset>
-		<legend>Login</legend>
 		<div class=loginIcon><img src="res/zombie-icon.png" alt="Zombie-Icon damit man daneben den Benutzernamen eingibt"></div>
 		<input type="text" name="username" placeholder="Username"/>
 		<div class=loginIcon><img src="res/lock-icon.png" alt="Schloss-Icon damit man daneben das Passwort eingibt"></div>
