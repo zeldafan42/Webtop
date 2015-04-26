4c0ff4a7076491f32e4d46369add1f186c9a19de
Zombieranke <abcdefghijklmnop@qrs>
2015-04-26 14:31:46 +0200

    The style configurations should now be saved in the position table and can be called with the getStyle() function which returns a CSS string; the apps already call this function

--- positionHandler.php
+++ positionHandler.php
@@ -35,30 +35,110 @@
 			$stmt->close();
 			
 			
-			/*$sqlCommand = "UPDATE position
-							SET width=? height=? top=? right=? bottom=? left=?
-							WHERE id=".$id." AND appname=?";
+			
+			$sqlCommand = "INSERT INTO position (id, appname, width, height, topoffset, rightoffset, bottomoffset, leftoffset) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+							ON DUPLICATE KEY UPDATE width=Values(?) height=VALUES(?) topoffset=VALUES(?) rightoffset=VALUES(?) bottomoffset=VALUES(?) leftoffset=VALUES(?)";
 			
 			$entry = $connect->prepare($sqlCommand);
-			$entry->bind_param('iiiiiis', $width, $height, $top, $right, $bottom, $left, $appname);
-			*/
 			
 			
-			
-			/*$sqlCommand = "INSERT INTO position (id, appname, width, height, topoffset, rightoffset, bottomoffset, leftoffset) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-							ON DUPLICATE KEY UPDATE id=VALUES(?)";
-			$entry = $connect->prepare($sqlCommand);
-			
-			
-			$entry->bind_param('isiiiiii', $id, $appname, $width, $height, $top, $right, $bottom, $left);
+			$entry->bind_param('isiiiiiiiiiiii', $id, $appname, $width, $height, $top, $right, $bottom, $left, $width, $height, $top, $right, $bottom, $left);
 				
 			if($entry->execute())
 			{
-				echo "<p>Datensatz wurde erfolgreich hinzugefügt, muss aber noch freigegeben werden</p>";
-			}*/
+				echo "<p>Popupdata was updated</p>";
+			}
 		
 		}
 		mysqli_close($connect);
+	}
+	
+	function getStyle($appname)
+	{
+		
+		$connect = mysqli_connect ("localhost", "root", "password","brunnhilde");
+		
+		if(mysqli_connect_errno() == 0)
+		{
+			echo "<p>Verbindung wurde aufgebaut</p>";
+				
+			$stmt = $connect->prepare("SELECT id FROM user WHERE username = ?");
+			$stmt->bind_param('s', $_SESSION['username']);
+				
+			$stmt->execute();
+			$stmt->bind_result($id);
+			$stmt->fetch();
+			$stmt->close();
+			
+			$stmt2 = $connect->prepare("SELECT closed FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($closed);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$stmt2 = $connect->prepare("SELECT width FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($width);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$stmt2 = $connect->prepare("SELECT height FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($height);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$stmt2 = $connect->prepare("SELECT topoffset FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($top);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$stmt2 = $connect->prepare("SELECT rightoffset FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($right);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$stmt2 = $connect->prepare("SELECT bottomoffset FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($bottom);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$stmt2 = $connect->prepare("SELECT leftoffset FROM position WHERE id = ? AND appname = ?");
+			$stmt2->bind_param('ss', $id, $appname);
+			$stmt2->execute();
+			$stmt2->bind_result($left);
+			$stmt2->fetch();
+			$stmt2->close();
+			
+			$outputString = "";
+			
+			if($closed == 0)
+			{
+				$outputString = $outputString + "position: absolute !important; ";
+				
+				$outputString = $outputString + "width: ".$width."px; ";
+				$outputString = $outputString + "height: ".$height."px; ";
+				$outputString = $outputString + "top: ".$top."px; ";
+				$outputString = $outputString + "right: ".$width."px; ";
+				$outputString = $outputString + "bottom: ".$bottom."px; ";
+				$outputString = $outputString + "left: ".$left."px;";
+			}
+			else
+			{
+				$outputString = $outputString + "display: none;";
+			}
+			return $outputString;
+		}
+			
 	}
 	
 	
--- webtop.php
+++ webtop.php
@@ -34,19 +34,19 @@
 			echo "<h1> Welcome ".$_SESSION['username']."</h1>";
 		?>
 		
-		<div id="waterIcon" class="webtopIcon" <?php if(isset($_SESSION['waterIcon'])){echo "style=\"".$_SESSION['waterIcon']."\"";}?>>
+		<div id="waterIcon" class="webtopIcon" <?php if(isset($_SESSION['waterIcon'])){echo "style=\"".getStyle(fotoApp)."\"";}?>>
 			<a href="index.php?fotoApp=true"> <img src="res/water-icon.png" alt="Wasser-Icon" > </a>
 		</div>
 		
-		<div id="fireIcon" class="webtopIcon" <?php if(isset($_SESSION['fireIcon'])){echo "style=\"".$_SESSION['fireIcon']."\"";}?>>
+		<div id="fireIcon" class="webtopIcon" <?php if(isset($_SESSION['fireIcon'])){echo "style=\"".getStyle(fireApp)."\"";}?>>
 			<a href="index.php?fireIcon=true"> <img src="res/fire-icon.png" alt="Feuer-Icon" > </a>
 		</div>
 		
-		<div id="leafIcon" class="webtopIcon" <?php if(isset($_SESSION['leafIcon'])){echo "style=\"".$_SESSION['leafIcon']."\"";}?>>
+		<div id="leafIcon" class="webtopIcon" <?php if(isset($_SESSION['leafIcon'])){echo "style=\"".getStyle(leafApp)."\"";}?>>
 			<a href="index.php?leafIcon=true"> <img src="res/leaf-icon.png" alt="Blatt-Icon" > </a>
 		</div>
 		
-		<div id="butterflyIcon" class="webtopIcon" <?php if(isset($_SESSION['butterflyIcon'])){echo "style=\"".$_SESSION['butterflyIcon']."\"";}?>>
+		<div id="butterflyIcon" class="webtopIcon" <?php if(isset($_SESSION['butterflyIcon'])){echo "style=\"".getStyle(butterflyApp)."\"";}?>>
 			<a href="index.php?butterflyIcon=true"> <img src="res/butterfly-icon.png" alt="Schmetterling-Icon" > </a>
 		</div>
 		
