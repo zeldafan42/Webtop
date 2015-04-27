f1a3821d81651c0ad444197b4c9e0b71859a2c94
Zombieranke <abcdefghijklmnop@qrs>
2015-04-27 15:52:18 +0200

    added generateRandomString($length) function in loginFunctions

--- loginFunctions.php
+++ loginFunctions.php
@@ -106,4 +106,17 @@ function correctInput(&$array)
 	return $array;
 	
 }
+
+function generateRandomString($length)
+{
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-_!"§$%&/+*~';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	
+	for ($i = 0; $i < $length; $i++)
+	{
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
+}
 ?>
